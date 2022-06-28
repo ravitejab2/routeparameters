40ad00e980234da3eaf70b593ae713effa7d0162
ravitejabollineni555@gmail.com <ravitejabollineni555@gmail.com>
2022-06-28 11:03:47 +0530

    observer way of extracting parameters

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -9,10 +9,18 @@ import { ActivatedRoute } from '@angular/router';
 export class OrderComponent implements OnInit {
 
   id:number=-1;
+  observerId:any;
   constructor(private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    //Snapshot way
     this.id= this.route.snapshot.params['id'];
+
+    //Observer way
+     this.route.paramMap.subscribe(params=>{
+      this.observerId= params.get('id');
+    });
+
   }
 
 }
